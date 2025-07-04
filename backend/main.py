69068bd5eae6c0bdcf66bf57e20c73691b1eef26
Mount Wario <y4859.rikoo_phy-sur21@outlook.jp>
2025-07-04 22:00:16 +0900

    fix requirements.txt, add CORSMiddleware

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import theme
 
 app = FastAPI()
 app.include_router(theme.router, prefix="/theme")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # フロントのURL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
