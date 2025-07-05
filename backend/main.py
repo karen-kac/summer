8624fd77eb842cc2a4c8003bd1c2838bcc932986
aoaovix <aoi0610vix@gmail.com>
2025-07-05 15:43:08 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -19,3 +19,7 @@ app.include_router(theme.router, prefix="/theme")
 @app.get("/")
 def read_root():
     return {"message": "Hello FastAPI!"}
+
+
+#python -m uvicorn main:app --reload
+#http://localhost:8000
